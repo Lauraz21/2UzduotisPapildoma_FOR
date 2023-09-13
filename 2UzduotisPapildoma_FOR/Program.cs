6b376c3ddfc6fee893927b9b80952718b864b1c7
Laura <Laura@DESKTOP-BESSB6E>
2023-09-13 23:04:59 +0300

    Add project files.

---
+++ 2UzduotisPapildoma_FOR/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _2UzduotisPapildoma_FOR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            for (int i = 1; i <= 100; i++)
+            {
+                
+                bool skaiciusPirminis = true;
+                int liek = 0;
+
+
+                for (int j = 2; j < i; j++)
+                {
+                    liek = i % j; 
+
+
+                    if (liek == 0)
+                    {
+                        skaiciusPirminis = false;
+                    }
+
+                }
+                if (skaiciusPirminis)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}   
